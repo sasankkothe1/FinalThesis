b7054aa4c1afd42f038205a8c1e5bdd5cc7dd507
Venkata Kothe <ga53xil@mytum.de>
2021-04-09 17:02:55 +0200

    about us page is completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="main-content">
+        <About />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./about.css";
+
+export default function About() {
+  return (
+    <div className="about-container">
+      <h4>PROJECT SCHEDULING PROBLEM LIBRARY - PSPLIB</h4>
+      <h5>What is it?</h5>
+      <p className="about-paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae
+        molestie ligula. Nunc sed libero rutrum, porta magna eu, malesuada diam.
+        Integer nunc justo, tempus non ultricies at, porttitor ut lectus. Ut
+        pharetra lorem vel urna bibendum, nec pulvinar purus porttitor. Fusce eu
+        turpis in eros tincidunt convallis vitae eu mi. Curabitur in nulla id
+        mauris interdum gravida. Vestibulum ante ipsum primis in faucibus orci
+        luctus et ultrices posuere cubilia curae; Cras sit amet justo at nulla
+        sollicitudin scelerisque quis vel ex. Phasellus viverra lorem purus, et
+        viverra urna pulvinar sed. Nullam consequat dolor sed mi rhoncus, vitae
+        viverra libero egestas.
+      </p>
+      <h5>How important is Project scheduling problem</h5>
+      <p className="about-paragraph">
+        Nunc ut porttitor nulla. Vestibulum ante ipsum primis in faucibus orci
+        luctus et ultrices posuere cubilia curae; Curabitur ultricies sapien
+        lectus, ac consequat tortor vestibulum eget. Mauris viverra, felis vitae
+        efficitur volutpat, justo justo pellentesque ante, at vestibulum sem
+        urna tincidunt erat. Nam imperdiet, ligula eu fermentum gravida, magna
+        nisi sodales mauris, nec viverra purus leo non ipsum. Maecenas euismod
+        enim dapibus, tempus ipsum quis, pellentesque tortor. Sed id ullamcorper
+        libero, a blandit velit. Phasellus leo ipsum, interdum in viverra
+        bibendum, placerat id magna. Donec non sapien mi. Pellentesque in
+        viverra nisi. Proin finibus fermentum mattis. Fusce iaculis massa non
+        auctor blandit.
+      </p>
+      <h5>Relavant research papers</h5>
+      <ul>
+        <li className="research-list-item">
+          Nunc ut porttitor nulla. Vestibulum ante ipsum primis in faucibus orci
+          luctus et ultrices posuere cubilia curae.
+        </li>
+        <li className="research-list-item">
+          Curabitur ultricies sapien lectus, ac consequat tortor vestibulum
+          eget.
+        </li>
+        <li className="research-list-item">
+          Mauris viverra, felis vitae efficitur volutpat, justo justo
+          pellentesque ante
+        </li>
+        <li className="research-list-item">
+          at vestibulum sem urna tincidunt erat. Nam imperdiet, ligula eu
+          fermentum gravida
+        </li>
+      </ul>
+    </div>
+  );
+}
